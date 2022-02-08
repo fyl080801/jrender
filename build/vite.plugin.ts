8c385f3efb30fd0fd65806eec451a5c9337e7e1e
fengyuanliang3 <fengyuanliang2@jd.com>
2022-02-08 10:19:54 +0800

    chore: package upgrade

--- build/vite.plugin.ts
+++ build/vite.plugin.ts
@@ -1,23 +1,10 @@
 import { createVuePlugin } from "vite-plugin-vue2";
 import ViteComponents from "vite-plugin-components";
-import ViteIcons, { ViteIconsResolver } from "vite-plugin-icons";
 import ScriptSetup from "unplugin-vue2-script-setup/vite";
 import legacy from "@vitejs/plugin-legacy";
 import WindiCSS from "vite-plugin-windicss";
 
 export const viteVue = createVuePlugin();
-
-export const viteComponents = ViteComponents({
-  customComponentResolvers: [
-    ViteIconsResolver({
-      componentPrefix: "",
-    }),
-  ],
-});
-
-export const viteIcon = ViteIcons({
-  defaultStyle: "",
-});
 
 export const scriptSetup = ScriptSetup({});
 
@@ -28,4 +15,4 @@ export const viteLegacy = legacy({
 
 export const windicss = WindiCSS();
 
-export const plugins = [viteVue, viteComponents, viteIcon, scriptSetup, viteLegacy, windicss];
+export const plugins = [viteVue, scriptSetup, viteLegacy, windicss];
